1c67362ab74851e8da091b956d6649df34ed45e9
davidmgudeman <davidmgudeman@gmail.com>
2017-06-18 05:35:05 -0700

    added styling to the new courses

--- assignment_e/js/custom_jquery_code.js
+++ assignment_e/js/custom_jquery_code.js
@@ -2,7 +2,7 @@ $(function() {
 
     $("button#btn_1").on("click", function() {
         let inputTxt = $("div#course_name input").val();
-        $("#statement > h3").text(inputTxt);
+        $("#statement > h3").text("Course Entered: " + inputTxt);
         $("#statement > h3").css({
             "background-color": "green",
             "font-size": "2em",
@@ -13,12 +13,14 @@ $(function() {
     $("#courses").children().on("click", function() {
         let inputTxt = $("div#course_name input").val();
         inputTxt = $("<li>" + inputTxt + "</li>");
+        inputTxt.css("background-color", "yellow");
         $(this).after(inputTxt);
     });
 
     $("#courses").children().children().children().on("click", function(event) {
         let inputTxt = $("div#course_name input").val();
         inputTxt = $("<li>" + inputTxt + "</li>");
+        inputTxt.css("background-color", "yellow");
         $(this).after(inputTxt);
         event.stopPropagation();
     });
